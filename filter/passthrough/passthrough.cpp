c9feed4267318757d8b610d7a5047d99fdad6bc7
khkim <khk.dblab@gmail.com>
2019-03-25 18:50:44 +0900

    pcl-project

--- filter/passthrough/passthrough.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <pcl/point_types.h>
-#include <pcl/filters/passthrough.h>
-
-int main(int argc, char** argv){
-	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
-	pcl::PointCloud<pcl::P
-}
